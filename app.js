5074b448f6d6d7a1244c967118a7f3de4ca055f5
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-10 13:15:17 +0100

    understanding req and sending res

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit(); /*hard coding the server exit */
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
