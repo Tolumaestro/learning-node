eaf2cd1f65cfb78614b49312c3296bcfb96be69a
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-12 19:32:20 +0100

    filtering paths

--- app.js
+++ app.js
@@ -3,28 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  // console.log("This always runs");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/add-product", (req, res, next) => {
-  // console.log("In product middleware");
-  res.send(
-    "<form action='/product' method='POST'> <input type='text' name='title' /> <button type='submit'>Submit</button </form>"
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  // console.log("In main middleware");
-  res.send("<h1>Hello from express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
